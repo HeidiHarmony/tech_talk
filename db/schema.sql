49fffc39361a99ae0e705d710fd3e4a6078f08d1
HeidiHarmony <tenibrae@gmail.com>
2024-04-02 22:01:35 -0400

    Finally eradicated troublesome bugs in userRoutes and useController. Fixed database name in schema.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,3 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS tech_talk_db;
+
+CREATE DATABASE tech_talk_db;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,11 +5,11 @@ const UserController = require('../../controllers/api/userController');
 
 // Define routes for user-related requests
 
-router.post('/signup', UserController.signup);
-router.post('/signin', UserController.signin);
-router.get('/getAllUsers', UserController.getAllUsers);
-router.get('/getUser/:id', UserController.getUserById);
-router.put('/updateUser/:id', UserController.updateUserById);
-router.post('/logout', UserController.logout);
+router.post('/signup', UserController.signup); // Sign up a new user
+router.post('/signin', UserController.signin); // Sign in a registered user
+router.get('/getAllUsers', UserController.getAllUsers); // Get all users
+router.get('/getUser/:id', withAuth, UserController.getUser); // Get a user by id
+router.put('/updateUser/:id', withAuth, UserController.updateUser); // Update a user by id
+router.post('/logout', withAuth, UserController.logout); // Log out a user
 
 module.exports = router;
